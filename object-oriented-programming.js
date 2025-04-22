6f18d47ccb3b5a67d0f881304f0887e16af1056c
Sunny Lee <sunnyxyst@gmail.com>
2025-04-22 22:54:09 +0900

    constructor desc added

--- object-oriented-programming.js
+++ object-oriented-programming.js
@@ -1,7 +1,9 @@
 class Apple {
     color;
     weight;
-
+    // 생성자 - 클래스로 객체를 처음 생성시 자동실행되는 함수
+    // (color, weight) 정보를 받아 
+    // this.color, this.weight 라는 속성을 만들어준다.
     constructor(color, weight) {
         this.color = color;
         this.weight = weight;
@@ -15,7 +17,7 @@ class Apple {
         console.log('사과를 먹었어요.')
     }
 }
-
+// 호출방식인 new Apple로 정보가 전달되면 생성자 함수가 실행된다.
 const myApple = new Apple('빨강', 150);
 myApple.peel();
 myApple.eat();
